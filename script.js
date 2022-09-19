1453a66585869665e53e713c34ee1ee3fc7e3b61
GamerSh <93838417+GamerShabandari@users.noreply.github.com>
2022-09-19 20:27:43 +0200

    fixat cookies

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const menuBtn = document.querySelector('.menuBtn');
 const openMenu = document.querySelector('.openMenu');
+const cookiesBtnAccept = document.querySelector('.cookiesBtnAccept');
+const cookiesBtnDecline = document.querySelector('.cookiesBtnDecline');
+const cookiesContainer = document.querySelector('.cookiesContainer');
+
 
 
 menuBtn.addEventListener('click', () => {
     openMenu.classList.toggle('hideMe');
+});
+
+cookiesBtnAccept.addEventListener('click', () => {
+    cookiesContainer.remove();
+});
+
+cookiesBtnDecline.addEventListener('click', () => {
+    cookiesContainer.remove();
 });
